d1453f8f4e07c0c8d7b12d9eab0c1ec4d5da97db
Kulpot <kulpotwahu@gmail.com>
2023-05-01 11:16:42 +0800

    Add project files.

---
+++ ReflectionsProperty/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionsProperty
+{
+    class Class1
+    {
+        //[Category("Its a number")]
+        [Description("Its a number")]
+        public int Number { get; set; }
+    }
+}
---
+++ ReflectionsProperty/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReflectionsProperty
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Class1 class1 = new Class1();
+            PropertyInfo info = class1.GetType().GetProperty("Number");
+            this.Text = GetPropertyDescription(info);
+        }
+
+        private static string GetPropertyDescription(PropertyInfo info)
+        {
+            Attribute attrib = info.GetCustomAttribute(typeof(DescriptionAttribute));//(CategoryAttribute)
+
+            if (attrib != null)
+            {
+                return ((DescriptionAttribute)attrib).Description;//(CategoryAttribute)attrib).Category;
+            }
+            else return "";
+        }
+    }
+}
